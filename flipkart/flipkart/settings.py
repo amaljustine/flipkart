b4d034bb321139aaa4d92eb0e488ce89e51ab589
amaljustine <amaljustine123@gmail.com>
2026-01-07 10:28:20 +0530

    added allowed host

--- flipkart/flipkart/settings.py
+++ flipkart/flipkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_)5xifclwvpdzo7#s#c^c0l%#pqz#g#9v&rw1h3-fe7grwu#**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amaljustine.pythonanywhere.com']
 
 
 # Application definition
